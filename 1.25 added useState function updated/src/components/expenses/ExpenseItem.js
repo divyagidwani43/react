ff7835ef0b0f09854e6b339ba8d57c01fda08734
divya <divya@gmail.com>
2023-06-25 17:51:00 +0530

    added useState function updated

---
+++ 1.25 added useState function updated/src/components/expenses/ExpenseItem.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [any1, any2] = useState(props.title);
+  {
+    /* any1 = the value inside() || any2 = the value we will set will be equated to value in ()after its called  */
+  }
+  function Click() {
+    {
+      /*the click function changes title to updated when called */
+    }
+    any2("updated");
+    {
+      /*any2 must be called as a function */
+    }
+  }
+  const [price, now] = useState(props.amount);
+  function change() {
+    const b = Number(prompt("enter discount percent "));
+    now(price - ((b/100) * price));
+  }
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{any1}</h2>
+        {/*any1 is props.title */}
+        <div className="expense-item__price">${price}</div>
+      </div>
+      <button onClick={Click}>change title</button>
+      {/*now when we click the click function will run */}
+      <button onClick={change}>b1</button>
+    </Card>
+  );
+};
+//now here title must change when button is clicked but it wont
+export default ExpenseItem;
